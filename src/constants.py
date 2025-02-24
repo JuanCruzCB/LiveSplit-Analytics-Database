5e3085a44057dcbd17845783b734fb59943eba6d
Juan Cruz <juancasserab@gmail.com>
2025-02-24 15:25:44 -0300

    Dict not used anymore

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,2 @@
 DATE_FORMAT = "%d/%m/%y"
 TIME_FORMAT = "{:02}.{:03}"
-
-SPLITS_RUNNERS = [
-    {"name": "1. NG Pro", "runner": "sawken"},
-    {"name": "splits arcadan", "runner": "arcadan"},
-    {"name": "splits derek", "runner": "derek"},
-    {"name": "splits joker", "runner": "joker"},
-    {"name": "splits luis", "runner": "luis"},
-    {"name": "splits mateo", "runner": "mateo"},
-    {"name": "splits richy", "runner": "richy"},
-]
