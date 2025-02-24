1dfb57183809ef5da7d3702e65f136b45116955f
Juan Cruz <juancasserab@gmail.com>
2025-02-24 16:46:10 -0300

    Refactor main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import time
 from contextlib import contextmanager
 
+from pandas import DataFrame
+
 from re4drive_manager import RE4DriveManager
 from re4database_manager import RE4DatabaseManager
 from re4sheet_manager import RE4SheetManager
@@ -20,58 +22,81 @@ def measure_total_time():
     execution_times["The whole script"] = execution_time_formatted
 
 
+def get_splits() -> dict[str, str]:
+    drive_manager = RE4DriveManager()
+    return drive_manager.download_splits()
+
+
+def update_query_database(splits: dict[str, str]) -> None:
+    db_manager = RE4DatabaseManager()
+    db_manager.update_runners_tables(splits=splits)
+    db_manager.update_global_tables()
+
+    df_doorsplit_golds = db_manager.query_doorsplit_golds()
+    df_chapter_golds = db_manager.query_chapter_golds()
+    df_chapter_golds_by_doors = db_manager.query_chapter_golds_by_doors()
+    df_section_golds = db_manager.query_section_golds()
+    df_section_golds_by_chapters = db_manager.query_section_golds_by_chapters()
+    df_section_golds_by_doors = db_manager.query_section_golds_by_doors()
+    df_best_paces = db_manager.query_best_paces()
+    df_rng_patterns = db_manager.query_rng_patterns()
+    df_general_stats = db_manager.query_general_stats()
+    df_resets = db_manager.query_resets()
+    df_weekday_data = db_manager.query_weekday_data()
+
+    db_manager.close_connection()
+
+    return (
+        df_doorsplit_golds,
+        df_chapter_golds,
+        df_chapter_golds_by_doors,
+        df_section_golds,
+        df_section_golds_by_chapters,
+        df_section_golds_by_doors,
+        df_best_paces,
+        df_rng_patterns,
+        df_general_stats,
+        df_resets,
+        df_weekday_data,
+    )
+
+
+def update_sheet(dataframes: tuple[DataFrame]) -> None:
+    sheet_manager = RE4SheetManager()
+    sheet_manager.copy_doorsplits_to_sheet(doorsplits=dataframes[0])
+    sheet_manager.copy_chapters_to_sheet(
+        chapters=dataframes[1],
+        chapters_by_doors=dataframes[2],
+    )
+    sheet_manager.copy_sections_to_sheet(
+        sections=dataframes[3],
+        sections_by_chapters=dataframes[4],
+        sections_by_doors=dataframes[5],
+    )
+    sheet_manager.copy_paces_to_sheet(paces=dataframes[6])
+    sheet_manager.copy_rng_patterns_to_sheet(rng_patterns=dataframes[7])
+    sheet_manager.copy_general_stats_to_sheet(general_stats=dataframes[8])
+    sheet_manager.copy_resets_to_sheet(resets=dataframes[9])
+    sheet_manager.copy_weekday_data_to_sheet(weekday_data=dataframes[10])
+    sheet_manager.post_last_update()
+
+    if False:
+        url_village_graph = graph_village(excel=excel_files[9])
+        url_castle_graph = graph_castle(excel=excel_files[9])
+        url_island_graph = graph_island(excel=excel_files[9])
+
+        sheet_manager.copy_graphs_to_sheet(
+            url_village_graph=url_village_graph,
+            url_castle_graph=url_castle_graph,
+            url_island_graph=url_island_graph,
+        )
+
+
 def main() -> None:
     with measure_total_time():
-        db_manager = RE4DatabaseManager()
-        sheet_manager = RE4SheetManager()
-
-        drive_manager = RE4DriveManager()
-        splits = drive_manager.download_splits()
-
-        db_manager.update_runners_tables(splits=splits)
-        db_manager.update_global_tables()
-
-        df_doorsplit_golds = db_manager.query_doorsplit_golds()
-        df_chapter_golds = db_manager.query_chapter_golds()
-        df_chapter_golds_by_doors = db_manager.query_chapter_golds_by_doors()
-        df_section_golds = db_manager.query_section_golds()
-        df_section_golds_by_chapters = db_manager.query_section_golds_by_chapters()
-        df_section_golds_by_doors = db_manager.query_section_golds_by_doors()
-        df_best_paces = db_manager.query_best_paces()
-        df_rng_patterns = db_manager.query_rng_patterns()
-        df_general_stats = db_manager.query_general_stats()
-        df_resets = db_manager.query_resets()
-        df_weekday_data = db_manager.query_weekday_data()
-
-        db_manager.close_connection()
-
-        sheet_manager.copy_doorsplits_to_sheet(doorsplits=df_doorsplit_golds)
-        sheet_manager.copy_chapters_to_sheet(
-            chapters=df_chapter_golds,
-            chapters_by_doors=df_chapter_golds_by_doors,
-        )
-        sheet_manager.copy_sections_to_sheet(
-            sections=df_section_golds,
-            sections_by_chapters=df_section_golds_by_chapters,
-            sections_by_doors=df_section_golds_by_doors,
-        )
-        sheet_manager.copy_paces_to_sheet(paces=df_best_paces)
-        sheet_manager.copy_rng_patterns_to_sheet(rng_patterns=df_rng_patterns)
-        sheet_manager.copy_general_stats_to_sheet(general_stats=df_general_stats)
-        sheet_manager.copy_resets_to_sheet(resets=df_resets)
-        sheet_manager.copy_weekday_data_to_sheet(weekday_data=df_weekday_data)
-        sheet_manager.post_last_update()
-
-        if False:
-            url_village_graph = graph_village(excel=excel_files[9])
-            url_castle_graph = graph_castle(excel=excel_files[9])
-            url_island_graph = graph_island(excel=excel_files[9])
-
-            sheet_manager.copy_graphs_to_sheet(
-                url_village_graph=url_village_graph,
-                url_castle_graph=url_castle_graph,
-                url_island_graph=url_island_graph,
-            )
+        splits = get_splits()
+        dataframes = update_query_database(splits=splits)
+        update_sheet(dataframes=dataframes)
 
     for func_name, exec_time in execution_times.items():
         print(f"{func_name} took {exec_time}")
