6d2ab8aa8bda4136a2f3ccf2fc5066be19e95d84
Juan Cruz <juancasserab@gmail.com>
2025-05-16 23:02:37 -0300

    Delete .bat files (moved elsewhere).

--- bat files/delete_excels.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "TARGET_DIR=H:\Juan\3. Projects\GH Sawken\Python\UpdateGoldsGlobal\excels"
-
-:: Deletes all files in the directory (non-recursive)
-del /Q "%TARGET_DIR%\*.*"
-
-echo All files in %TARGET_DIR% have been deleted.
-pause
--- bat files/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "H:\Juan\3. Projects\GH Sawken\Python\UpdateGoldsGlobal"
-call .venv\Scripts\activate
-cd /d "H:\Juan\3. Projects\GH Sawken\Python\UpdateGoldsGlobal\src"
-python main.py
-pause
