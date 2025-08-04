4cbfa805886e06a0e2836018090898a014541fa0
Juan Cruz <juancasserab@gmail.com>
2025-08-03 21:37:40 -0300

    Remove commas from split names

--- src/re4splits_manager.py
+++ src/re4splits_manager.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from defusedxml.ElementTree import parse
 
 
+class SplitsFileParseError(Exception):
+    """Raised when a splits file cannot be parsed as expected."""
+
+
 class RE4SplitsManager:
     CURRENTLY_ALLOWED_SPLITS = (
         "splits luis.lss",
@@ -36,9 +40,7 @@ class RE4SplitsManager:
         returns a dict where each key is the splits file name and the value is
         that splits last modification time.
         """
-        if not self._splits_output_folder.exists():
-            msg = "The output folder for the splits of other runners does not exist."
-            raise FileNotFoundError(msg)
+        self._check_splits_folder_existence()
 
         local_splits = {
             splits_file.stem: datetime.fromtimestamp(
@@ -54,27 +56,47 @@ class RE4SplitsManager:
         return local_splits
 
     def clean_splits(self) -> None:
-        if not self._splits_output_folder.exists():
-            msg = "The output folder for the splits of other runners does not exist."
-            raise FileNotFoundError(msg)
+        """
+        Go through each of the splits files and clean them to
+        allow the SQL script to parse them safely.
+
+        To clean them, first remove all data of the Icon tags, then
+        remove any commas from any split name, then ???
+        """
+        self._check_splits_folder_existence()
 
         for splits_file in self._splits_output_folder.glob(pattern="*.lss"):
-            splits_are_invalid = False
+            splits_are_dirty = False
             print(f"Checking '{splits_file.name}'...", end="")
             splits_file_tree = parse(splits_file)
             root = splits_file_tree.getroot()
+
+            if root is None:
+                msg = f"An unexpected error ocurred while inspecting '{splits_file.name}'."
+                raise SplitsFileParseError(msg)
 
             # 1. Ensure the splits don't have any icons
             for icon in root.findall(".//Icon"):
                 if len(icon) > 0 or icon.text:
                     icon.clear()
-                    splits_are_invalid = True
+                    splits_are_dirty = True
 
-            if splits_are_invalid:
-                print(" Splits are invalid, cleaning them...", end="")
+            # 2. Remove commas from split names
+            for name in root.findall(".//Name"):
+                if name.text is not None and "," in name.text:
+                    name.text = name.text.replace(",", "|")
+                    splits_are_dirty = True
+
+            if splits_are_dirty:
+                print(" Splits are dirty, cleaning them...", end="")
                 splits_file_tree.write(
                     splits_file,
                     encoding="utf-8",
                     xml_declaration=True,
                 )
             print()
+
+    def _check_splits_folder_existence(self):
+        if not self._splits_output_folder.exists():
+            msg = "The output folder for the splits of other runners does not exist."
+            raise FileNotFoundError(msg)
