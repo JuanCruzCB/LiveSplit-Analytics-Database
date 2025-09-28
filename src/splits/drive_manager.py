e4c19a814dfac1f2e009a3e1f9cfcfbca058c492
Juan Cruz <juancasserab@gmail.com>
2025-09-28 19:03:36 -0300

    DriveManager cleanup

--- src/splits/drive_manager.py
+++ src/splits/drive_manager.py
@@ -32,19 +32,24 @@ class DriveManager:
         yet exist locally because they haven't been downloaded yet.
         """
         logger.info("Downloading any out-of-sync split files from the Drive...")
+        for remote_file in self._list_remote_files():
+            if "splits" in remote_file["title"]:
+                self._process_remote_splits_file(remote_file)
+
+    def _list_remote_files(self) -> list[GoogleDriveFile]:
+        """
+        List all non-trashed files in the Google Drive folder specified
+        in the constructor.
+        """
         query = {"q": f"'{self._google_drive_folder_id}' in parents and trashed=false"}
         try:
-            all_remote_files = self._google_drive.ListFile(query).GetList()
+            return self._google_drive.ListFile(query).GetList()
         except ApiRequestError as e:
             logger.exception(
                 "There's no Google Drive folder with id = %s",
                 self._google_drive_folder_id,
             )
             raise GoogleDriveFolderNotFoundError from e
-
-        for remote_file in all_remote_files:
-            if "splits" in remote_file["title"]:
-                self._process_remote_splits_file(remote_file)
 
     def _process_remote_splits_file(
         self,
@@ -56,64 +61,60 @@ class DriveManager:
         If the runner name contained within the splits file's name is not
         in the list of allowed runners → ignore the file.
 
-        If the runner name is in the list of allowed runners but doesn't exist
-        locally yet → download the file.
+        If it is in the list → check whether the file exists locally.
 
-        If it doesn't → download the file.
+        If it doesn't → download the file for the first time.
 
-        If it does → check whether the local version is outdated and if so,
-        download the remote version to sync it locally.
+        If it does AND is out of sync → download the file to sync it.
         """
         file = remote_file["title"]
         runner_name = file.replace(".lss", "").replace("splits ", "")
 
         if runner_name not in self._splits_manager.allowed_runners:
-            return logger.warning(
+            logger.warning(
                 "Ignoring unknown splits file: '%s'",
                 file,
             )
+            return None
 
-        matching_local_splits_file = (
-            self._splits_manager.find_splits_file_by_runner_name(
-                runner_name=runner_name,
-            )
-        )
-        if matching_local_splits_file is None:
-            return self._download_drive_splits(remote_file, first_time=True)
+        local_file = self._splits_manager.find_splits_file_by_runner_name(runner_name)
 
-        if matching_local_splits_file.is_outdated(
+        if local_file is None:
+            return self._download_file(remote_file, first_time=True)
+
+        if local_file.is_outdated(
             datetime.strptime(
                 remote_file["modifiedDate"],
                 self.GOOGLE_DRIVE_DATE_TIME_FORMAT,
             ).replace(tzinfo=UTC),
         ):
-            return self._download_drive_splits(remote_file)
+            return self._download_file(remote_file)
 
         return logger.info(
             (
                 "Splits file '%s' is already up to date locally, "
                 "so there's no need to update it."
             ),
-            matching_local_splits_file.file_path.stem,
+            local_file.file_path.stem,
         )
 
-    def _download_drive_splits(
+    def _download_file(
         self,
-        splits_file: GoogleDriveFile,
+        file: GoogleDriveFile,
         *,
         first_time: bool = False,
     ) -> None:
         """
-        Download the specified splits_file from the Google Drive folder
+        Download the specified file from the Google Drive folder
         into the folder where all the splits files are stored locally.
 
-        Log a different message depending on whether this splits file
+        Log a different message depending on whether this file
         is being downloaded for the first time ever or not.
         """
-        filename = splits_file["title"]
+        filename = file["title"]
         logger.info(
             "Downloading '%s'%s...",
             filename,
             " for the first time" if first_time else "",
         )
-        splits_file.GetContentFile(self._splits_manager.splits_folder / filename)
+        file.GetContentFile(self._splits_manager.splits_folder / filename)
